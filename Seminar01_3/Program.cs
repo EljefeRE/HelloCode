2d9fa0a762ca9b143e5d988173bdda127d0a33a7
Sergey <isergeytarhov@gmail.com>
2022-09-30 22:31:41 +0300

    add Seminar 1 3

---
+++ Seminar01_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 100 && n < 1000)
+{
+    Console.Write($"{n % 10}\n");
+}
+else
+{
+    Console.WriteLine("Введите трехзначное число!");
+}
