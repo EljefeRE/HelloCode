ce1baf2578b0f0b82f191573997511866c81e017
Sergey <isergeytarhov@gmail.com>
2022-09-30 22:23:08 +0300

    add Seminar 1 3

---
+++ Seminar01_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите положительное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = n - n * 2;
+
+Console.Write($"{i}\n");
+while (i < n)
+{
+    i++;
+    Console.Write($"{i}\n");
+}
