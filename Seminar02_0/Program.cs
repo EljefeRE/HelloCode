acccb0992cc7ce8a3461e4e41e53f501af9d032b
Sergey <isergeytarhov@gmail.com>
2022-10-04 22:18:12 +0300

    add Seminar02 0 ok

---
+++ Seminar02_0/Program.cs
@@ -0,0 +1,16 @@
+﻿int num = new Random().Next(10,100);
+int num1 = num / 10;
+int num2 = num % 10;
+
+Console.WriteLine(num);
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.WriteLine(num2);
+}
